b020379ac111cebb1243ae042372220c35b92cbb
Pooja pooja <poojitha6789012@gmail.com>
2023-02-20 00:12:48 +0530

    Added HashMap to Store Calculation History in hashmap

--- src/main/java/com/myproject/calculator/CalculatorHistory.java
+++ src/main/java/com/myproject/calculator/CalculatorHistory.java
@@ -1,13 +1,16 @@
 package com.myproject.calculator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class CalculatorHistory {
     private List<Calculation> history = new ArrayList<>();
+    private HashMap<String,Double> historyInHashMap=new HashMap<>();
 
     public void addCalculation(Calculation calculation) {
         history.add(calculation);
+        historyInHashMap.put(calculation.getInput(),calculation.getResult());
     }
 
     public List<Calculation> getHistory() {
